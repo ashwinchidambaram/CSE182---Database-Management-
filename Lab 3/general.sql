5a329fc0f462d6657d3c3fd638a636e8d2ef5f3e
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2020-05-17 11:37:54 -0700

    fix

--- Lab 3/general.sql
+++ Lab 3/general.sql
@@ -6,4 +6,4 @@ ALTER TABLE Species ADD CONSTRAINT ani CHECK (animalCount >= 0);
 ALTER TABLE Members ADD CHECK(joinDate < expirationDate);
 
 -- Part 3
-ALTER TABLE Keepers ADD CHECK(keeperLevel IS NULL OR keeperSalary IS NULL);
+ALTER TABLE Keepers ADD CHECK(keeperLevel IS NOT NULL OR keeperSalary IS NULL);
